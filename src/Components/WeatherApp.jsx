23b93e9076c32c7dba94fa826a91e40ae4d66a5d
Nick Rabone <n.rabone72@aol.com>
2024-08-10 20:40:15 +0100

    Creating search bar

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -3,7 +3,14 @@ const WeatherApp = () => {
     <div className='container'>
       <div className="weather-app">
         <div className="search">
-          <div className="search-top"></div>
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot"></i>
+            <div className="location">Birmingham</div>
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Enter placename..." />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
         </div>
       </div>      
     </div>
