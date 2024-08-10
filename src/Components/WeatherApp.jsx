d239921ce007dc6aeb9e07ea3cdec88778d65924
Nick Rabone <n.rabone72@aol.com>
2024-08-10 21:12:28 +0100

    Completing JSX for WeatherApp.jsx.

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,3 +1,8 @@
+import sunny from '../assets/images/sunny.png'
+import cloudy from '../assets/images/cloudy.png'
+import rainy from '../assets/images/rainy.png'
+import snowy from '../assets/images/snowy.png'
+
 const WeatherApp = () => {
   return (
     <div className='container'>
@@ -10,6 +15,26 @@ const WeatherApp = () => {
           <div className="search-bar">
             <input type="text" placeholder="Enter placename..." />
             <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+        <div className="weather">
+          <img src={sunny} alt="sunny" />
+          <div className="weather-type">Clear</div>
+          <div className="temp">28°</div>
+        </div>
+        <div className="weather-date">
+          <p>Saturday 10 August</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">39%</div>
+          </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">3km/h</div>
           </div>
         </div>
       </div>      
